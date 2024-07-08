65475f551e25b07d386b36f1025c90b27dae80ec
Zafar <umerov.z99>
2024-07-08 15:32:50 +0500

    New Project

---
+++ src/util.js
@@ -0,0 +1,12 @@
+export const headersGet = {
+    'accept': 'application/json',
+    'authorization': 'Basic YWRtaW4xMjM6YWRtaW4xMjM=',
+    'X-CSRFToken': 'PbaUeeNc2YT26KnCK4blwmtG5Q8ce7nqhpKx1d9Unn6xxEqKGsuOcUjWpe0YtAku'
+}
+
+
+export const url = 'http://estetiktest.ru/api/'
+export const urlClinicks = 'http://estetiktest.ru/api/clinics/'
+export const common = 'http://estetiktest.ru/api/common/config/'
+export const product = 'http://estetiktest.ru/api/products/'
+export const services = 'http://estetiktest.ru/api/services/'
